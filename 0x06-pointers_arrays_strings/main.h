03b82eac6fd453fa534d9666e68a5f50a8759f96
ahmed_fadel <ahmed.mahmoud.fadel@gmail.com>
2023-03-05 13:26:08 -0800

    tasks

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+char *leet(char *);
+void print_number(int n);
+
+#endif
