954b036386a0d72c88a897ff2849ae7cca84c54f
ahmed_fadel <ahmed.mahmoud.fadel@gmail.com>
2023-03-04 21:35:26 -0800

    betty code

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,20 +10,20 @@
 
 void print_square(int size)
 {
-    int i, j;
+  int i, j;
 
-    if (size <= 0)
+  if (size <= 0)
     {
-        _putchar('\n');
-        return;
-    }
+    _putchar('\n');
+    return;
+  }
 
-    for (i = 0; i < size; i++)
+  for (i = 0; i < size; i++)
     {
-        for (j = 0; j < size; j++)
-        {
-            _putchar('#');
-        }
-        _putchar('\n');
+    for (j = 0; j < size; j++)
+      {
+      _putchar('#');
     }
+    _putchar('\n');
+  }
 }
